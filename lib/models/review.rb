6c260c9aa37491ed9eef2b12424c296de25e4f73
Daniel Weber <daniel@bitlathe.com>
2014-09-17 11:12:08 -0400

    Removed unused files

--- lib/models/review.rb
+++
@@ -1,7 +0,0 @@
-class Review < ActiveRecord::Base
-  attr_accessible :beer_name,:maker,:drink_again,:rating,:comments,:date_added
-
-  class Entity < Grape::Entity
-    expose :id,:beer_name,:maker,:drink_again,:rating,:comments,:date_added
-  end
-end
